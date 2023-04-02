6370d9944f68ba49d80aeb17571003490faa91fc
akshay <akshaypvkeerakkal@gmail.com>
2023-04-02 18:59:34 +0530

    landing page completed

--- client/src/pages/admin/AdminLogin.jsx
+++ client/src/pages/admin/AdminLogin.jsx
@@ -8,7 +8,7 @@ function AdminLogin() {
     const token = localStorage.getItem("adminAuthToken");
     if (token) return navigate("/admin/dashboard");
   }, [navigate]);
-  return <Login role={"Admin"} url={"https://img.freepik.com/free-vector/competent-resume-writing-professional-cv-constructor-online-job-application-profile-creation-african-american-woman-filling-up-digital-form-concept-illustration_335657-2053.jpg?size=626&ext=jpg&ga=GA1.1.1478787354.1678959639&semt=ais"} />;
+  return <Login role={"Admin"} url={"/adminlogin.webp"} />;
 }
 
 export default AdminLogin;
